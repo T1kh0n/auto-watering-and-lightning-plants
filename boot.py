c7409e9841d9435a1867bfb8009ae3d9934ea67f
T1kh0n <85391163+T1kh0n@users.noreply.github.com>
2023-03-19 21:53:12 +0200

    Update boot.py

--- boot.py
+++ boot.py
@@ -2,6 +2,10 @@ import machine
 import network
 import time
 
+
+# подключение кнопки
+button = machine.Pin(13, machine.Pin.IN, machine.Pin.PULL_UP)
+
 # подключение реле
 # досветка
 relay_1 = machine.Pin(26, machine.Pin.OUT)
@@ -9,23 +13,28 @@ relay_1.value(0)
 
 
 def connect():
-    wlan = network.WLAN(network.STA_IF)
+	wlan = network.WLAN(network.STA_IF)
 
-    if not wlan.isconnected():
-        print('Connecting to network...')
+	if not wlan.isconnected():
+		print('Connecting to network...')
 
-        wlan.active(True)
-        wlan.connect('', '')
+		wlan.active(True)
+		wlan.connect('ASUS_30', 'giant_5447')
 
-    print('Network config: ', wlan.ifconfig())
-count = 0
+		while not wlan.isconnected():
+			pass
 
-try:
-	connect()
-except:
-	print('Error')
-	for i in range(10):
-		relay_1.value(1)
-		time.sleep(3)
-		relay_1.value(0)
-		time.sleep(3)
+	print('Network config: ', wlan.ifconfig())
+
+
+if __name__ == '__main__':
+	try:
+		connect()
+	except:
+		while True:
+			if butt0n.value() == 0:
+				break
+			relay_1.value(1)
+			time.sleep(3)
+			relay_1.value(0)
+			time.sleep(6)
